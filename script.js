85d76ae5762deab6c3869e05a710327756b0465c
Wildercat <abramgornik@gmail.com>
2019-09-12 18:12:19 -0400

    working mvp

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ var state = 0;
 var states = [0,1,2,3,4,5];
 var reader = document.getElementById('reader');
 
-function post(h1,bText,p1) {
+function post(h1,bText,p1, symbBoolean, symb) {
     //Clear the div
     reader.innerHTML = '';
 
     let newH1 = document.createElement('h1');
     newH1.textContent = h1;
+    if (symbBoolean){
+        newH1.appendChild(symb);
+    }
     reader.appendChild(newH1);
     
     let newButton = document.createElement('button');
@@ -49,7 +52,7 @@ function page0() {
     let h1 = 'I can read your mind.';
     let bText = 'Begin';
     state = 1;
-    post(h1,bText,'');
+    post(h1,bText,'',false,'');
 }
 
 function page1() {
@@ -57,15 +60,15 @@ function page1() {
     let bText = 'Next';
     let p1 = 'When you have your number, click next.';
     state++;
-    post(h1,bText,p1);
+    post(h1,bText,p1,false,'');
 }
 
 function page2() {
-    let h1 = 'Add both numbers together to get a new number';
+    let h1 = 'Add both digits together to get a new number';
     let bText = 'Next';
     let p1 = 'Ex: 14 is 1 + 4 = 5';
     state++;
-    post(h1,bText,p1);
+    post(h1,bText,p1,false,'');
 }
 
 function page3() {
@@ -73,7 +76,7 @@ function page3() {
     let bText = 'Next';
     let p1 = 'Ex: 14 - 5 = 9';
     state++
-    post(h1,bText,p1);
+    post(h1,bText,p1,false,'');
 }
 
 function page4() {
@@ -84,17 +87,25 @@ function page4() {
     }
     //print out the numbers with random symbols attached to them
     //wow I should have done this with the ordered list tag
-    uList = document.createElement('ul');
-    
+    var uList = document.createElement('ul');
     for (let i = 0; i < 100; i++) {
-
+        if (numList[i] % 9 == 0) {
+            let li9 = document.createElement('li');
+            li9.textContent = numList[i] + ': &';
+            uList.appendChild(li9);
+        } else {
+            let li = document.createElement('li');
+            li.textContent = numList[i] + ': ' + rSymbol();
+            uList.appendChild(li);
+        }
     }
+    
 
     let h1 = 'NUMBERLIST';
     let bText = 'Reveal';
     let p1 = 'Find your new number. Note the symbol beside the number.';
     state++
-    post(h1,bText,p1);     
+    post(h1,bText,p1,true,uList);     
 }
 
 function page5() {
@@ -102,7 +113,14 @@ function page5() {
     let bText = 'Reset';
     let p1 = 'Was this your symbol?';
     state++
-    post(h1,bText,p1);     
+    post(h1,bText,p1);
 }
+
+//returns a random symbol
+function rSymbol() {
+    let symbolList = ['!','@','#','$','%','^','*','+'];
+    return symbolList[Math.floor(Math.random() * symbolList.length)];
+}
+
 
 page0();
